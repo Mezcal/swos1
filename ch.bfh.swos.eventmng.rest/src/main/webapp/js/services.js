cfb3ccf947d5318f2786a162583f42cf069297c2
AdrianM <adrian.morgenegg@gmail.com>
2012-12-21 11:59:33 +0100

    fix update method (remove ID)

--- ch.bfh.swos.eventmng.rest/src/main/webapp/js/services.js
+++ ch.bfh.swos.eventmng.rest/src/main/webapp/js/services.js
@@ -4,6 +4,6 @@ var services = angular.module('services', ['ngResource']);
 
 services.factory('Event', function ($resource) {
     return $resource('rest/events/:id', {id: '@id'}, {
-        'update': {method: 'PUT'}
+        'update': {method: 'PUT',params: { id: null }}
     });
 });
