78f4fc67ccd71f2a21a30d88e23af648ceeb203a
AdrianM <adrian.morgenegg@gmail.com>
2013-01-08 22:02:47 +0100

    - add checkbox for acts and locations

--- ch.bfh.swos.eventmng.rest/src/main/webapp/js/controllers.js
+++ ch.bfh.swos.eventmng.rest/src/main/webapp/js/controllers.js
@@ -2,11 +2,16 @@
 
 var controllers = angular.module('controllers', ['services']);
 
-function EventController($scope, Event) {
+function EventController($scope, Event,Act,Location) {
     $scope.currentEvent = new Event();
+    $scope.currentEventActs = {};
+    $scope.currentEventLocations = {};
     $scope.events = Event.query();
+    $scope.acts = Act.query();
+    $scope.locations = Location.query();
     $scope.showId = false;
-
+    $scope.debugMsg = "";
+    
     $scope.cancel = function () {
         $scope.currentEvent = new Event();
     };
@@ -23,11 +28,46 @@ function EventController($scope, Event) {
     };
 
     $scope.edit = function (event) {
+    	$scope.currentEventActs = $scope.loadSelection(event.acts, $scope.currentEventActs);
+    	$scope.currentEventLocations = $scope.loadSelection(event.locations, $scope.currentEventLocations);
         $scope.currentEvent = event;
     };
 
     $scope.remove = function (index, id) {
 		$scope.events.splice(index, 1);
 		Event.remove({'id':id});
+    };
+    
+    $scope.selectAct = function(act){
+    	$scope.debug("selected");
+    	return true;
+    };
+    
+    /**
+     * load selection
+     */
+    $scope.loadSelection = function(pItems,pSelectionArray){
+    	pSelectionArray = {};
+    	jQuery.each(pItems,function(){
+    		pSelectionArray[this.id] = true;
+    	});
+    	return pSelectionArray;
+    };
+    
+    /**
+     * check if ID is the array
+     */
+    $scope.inArrayId = function (pArray,pId){
+    	result = jQuery.grep(pArray, function(value) {
+    		return value.id == pId;
+    	});
+    	return result.length === 0;
+    };
+    
+    /**
+     * debug
+     */
+    $scope.debug = function(msg){
+    	$scope.debugMsg = $scope.debugMsg+" | "+msg;
     };
 }
--- ch.bfh.swos.eventmng.rest/src/main/webapp/js/services.js
+++ ch.bfh.swos.eventmng.rest/src/main/webapp/js/services.js
@@ -2,8 +2,29 @@
 
 var services = angular.module('services', ['ngResource']);
 
+/**
+ * REST service for Event
+ */
 services.factory('Event', function ($resource) {
     return $resource('rest/events/:id', {id: '@id'}, {
+        'update': {method: 'PUT',params: { id: null }}
+    });
+});
+
+/**
+ * REST service for Act
+ */
+services.factory('Act', function ($resource) {
+    return $resource('rest/acts/:id', {id: '@id'}, {
+        'update': {method: 'PUT',params: { id: null }}
+    });
+});
+
+/**
+ * REST service for Location
+ */
+services.factory('Location', function ($resource) {
+    return $resource('rest/locations/:id', {id: '@id'}, {
         'update': {method: 'PUT',params: { id: null }}
     });
 });
