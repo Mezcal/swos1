1de4f61e628ba03de474b219586d886609d57be3
AdrianM <adrian.morgenegg@gmail.com>
2013-01-08 23:31:33 +0100

    - checkbox-select for location and act workd (with DB access)

--- ch.bfh.swos.eventmng.rest/src/main/webapp/js/controllers.js
+++ ch.bfh.swos.eventmng.rest/src/main/webapp/js/controllers.js
@@ -4,64 +4,103 @@ var controllers = angular.module('controllers', ['services']);
 
 function EventController($scope, Event,Act,Location) {
     $scope.currentEvent = new Event();
-    $scope.currentEventActs = {};
-    $scope.currentEventLocations = {};
+    $scope.currentEventActs = [];
+    $scope.currentEventLocations = [];
     $scope.events = Event.query();
     $scope.acts = Act.query();
     $scope.locations = Location.query();
     $scope.showId = false;
     $scope.debugMsg = "";
     
+    /**
+     * cancel edit
+     */
     $scope.cancel = function () {
         $scope.currentEvent = new Event();
+        $scope.currentEventActs = [];
+    	$scope.currentEventLocations = [];
     };
 
+    /**
+     * save event
+     */
     $scope.save = function () {
-        var isNew = $scope.currentEvent.id == null;
-        if (isNew) {
-            $scope.currentEvent = Event.save($scope.currentEvent);
-            $scope.events.push($scope.currentEvent);
-        } else {
-            $scope.currentEvent = Event.update($scope.currentEvent);
-        }
-        $scope.cancel();
+        //if(eventForm.input.$valid){
+	    	var isNew = $scope.currentEvent.id == null;
+	        if (isNew) {
+	            $scope.currentEvent = Event.save($scope.currentEvent);
+	            $scope.events.push($scope.currentEvent);
+	        } else {
+	            $scope.currentEvent = Event.update($scope.currentEvent);
+	        }
+	        $scope.cancel();
+        //}
     };
 
+    /**
+     * edit selected event
+     */
     $scope.edit = function (event) {
     	$scope.currentEventActs = $scope.loadSelection(event.acts, $scope.currentEventActs);
     	$scope.currentEventLocations = $scope.loadSelection(event.locations, $scope.currentEventLocations);
         $scope.currentEvent = event;
     };
 
+    /**
+     * remove event
+     */
     $scope.remove = function (index, id) {
 		$scope.events.splice(index, 1);
 		Event.remove({'id':id});
     };
     
+    /**
+     * select act via checkbox
+     */
     $scope.selectAct = function(act){
-    	$scope.debug("selected");
-    	return true;
+    	if(jQuery.type($scope.currentEvent.acts) === "undefined"){
+    		$scope.debug("init act array!");
+    		$scope.currentEvent.acts = [];
+    	}
+    	
+    	if($scope.currentEventActs[act.id]){
+    		$scope.currentEvent.acts.push(act);
+    	}else{
+    		//remove value whit grep-function (return an array without the selected value)
+    		$scope.currentEvent.acts = jQuery.grep($scope.currentEvent.acts, function(value) {
+        		return value.id != act.id;
+        	});
+    	} 
+    };
+    
+    /**
+     * select act via checkbox
+     */
+    $scope.selectLocation = function(loc){
+    	if(jQuery.type($scope.currentEvent.locations) === "undefined"){
+    		$scope.debug("init location array!");
+    		$scope.currentEvent.locations = [];
+    	}
+    	
+    	if($scope.currentEventLocations[loc.id]){
+    		$scope.currentEvent.locations.push(loc);
+    	}else{
+    		//remove value whit grep-function (return an array without the selected value)
+    		$scope.currentEvent.locations = jQuery.grep($scope.currentEvent.locations, function(value) {
+        		return value.id != loc.id;
+        	});
+    	}
     };
     
     /**
      * load selection
      */
     $scope.loadSelection = function(pItems,pSelectionArray){
-    	pSelectionArray = {};
+    	pSelectionArray = [];
     	jQuery.each(pItems,function(){
     		pSelectionArray[this.id] = true;
     	});
     	return pSelectionArray;
-    };
-    
-    /**
-     * check if ID is the array
-     */
-    $scope.inArrayId = function (pArray,pId){
-    	result = jQuery.grep(pArray, function(value) {
-    		return value.id == pId;
-    	});
-    	return result.length === 0;
     };
     
     /**
