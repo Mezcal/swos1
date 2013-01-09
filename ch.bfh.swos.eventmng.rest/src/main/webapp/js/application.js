8709048f4c1d873db8f4391e5b2a9dc5fbb6b175
AdrianM <adrian.morgenegg@gmail.com>
2013-01-09 12:11:54 +0100

    - add twitter bootstrap - add template and partials - add route - add events.html

--- ch.bfh.swos.eventmng.rest/src/main/webapp/js/application.js
+++ ch.bfh.swos.eventmng.rest/src/main/webapp/js/application.js
@@ -1,4 +1,19 @@
 'use strict';
 
-var eventmng = angular.module('eventmng', ['controllers', 'services','directives']);
+var eventmng = angular.module('eventmng', ['controllers', 'services','directives']).config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+	when('/', {
+		templateUrl: 'partials/home.html'
+	}).
+	when('/acts', {
+		templateUrl: 'partials/acts.html'
+	}).
+	when('/locations', {
+		templateUrl: 'partials/locations.html'
+	}).
+	when('/events', {
+		templateUrl: 'partials/events.html'
+	}).
+      otherwise({redirectTo: '/'});
+}]);
 
